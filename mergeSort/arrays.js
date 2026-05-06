3355dff5d08187e9405da42241471bef7cd117b5
Mariele <mariele.florencio@escola.pr.gov.br>
2026-05-06 13:41:44 -0300

    Add files via upload

---
+++ mergeSort/arrays.js
@@ -0,0 +1,14 @@
+const listaCelular =[ 
+    {nome: "Samsung A14", preco:900},
+    {nome: "Moto G54", preco:1200},
+    {nome: "Iphone 11", preco:2500},
+    {nome: "Galaxy", preco:4000},
+    {nome: "Xiaome Note 12", preco:1000},
+    {nome: "Poco X5", preco:1300},
+    {nome: "Realme 11", preco:1500},
+    {nome: "Iphone 13", preco:3500},
+    {nome: "Iphone 14", preco:4500},
+    {nome: "Asus ROG Phone", preco:5000},
+];
+
+module.exports = listaCelular;
